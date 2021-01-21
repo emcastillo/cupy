8127bb7bf63b8d67f45c1bb89b571d8a6b41c7e0
Leo Fang <leofang@bnl.gov>
2021-01-21 12:38:15 -0500

    in favor of stream sync over device sync during alloc failure

--- cupy/cuda/memory.pyx
+++ cupy/cuda/memory.pyx
@@ -1332,7 +1332,7 @@ cdef class SingleDeviceMemoryPool:
         return mem
 
 
-cdef class MemoryPool(object):
+cdef class MemoryPool:
 
     """Memory pool for all GPU devices on the host.
 
@@ -1489,7 +1489,7 @@ cdef class MemoryPool(object):
         return mp.get_limit()
 
 
-cdef class MemoryAsyncPool(object):
+cdef class MemoryAsyncPool:
     """(Experimental) CUDA memory pool for all GPU devices on the host.
 
     A memory pool preserves any allocations even if they are freed by the user.
@@ -1580,13 +1580,14 @@ cdef class MemoryAsyncPool(object):
         except CUDARuntimeError as e:
             if e.status != runtime.errorMemoryAllocation:
                 raise
-            self.free_all_blocks()  # synchronize!
+            stream = stream_module.get_current_stream()
+            stream.synchronize()
             try:
                 mem = malloc_async(rounded_size)
             except CUDARuntimeError as e:
                 if e.status != runtime.errorMemoryAllocation:
                     raise
-                self.free_all_blocks()  # synchronize!
+                stream.synchronize()
                 try:
                     mem = malloc_async(rounded_size)
                 except CUDARuntimeError as e:
